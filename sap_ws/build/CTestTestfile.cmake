4400c327a4a312779806b0fe68025253fdf196c9
hyojin-shin <tlsgywls888@naver.com>
2021-05-06 03:22:38 +0900

    DB

--- sap_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/seonghee/Desktop/Capstone/sap_ws/src
-# Build directory: /home/seonghee/Desktop/Capstone/sap_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
