32e98b27d3a000b8e636f6e042bc8f3cd435d2d3
hyojin-shin <tlsgywls888@naver.com>
2021-06-23 16:00:33 +0900

    menu
    menu 추가

---
+++ sap_mode_menu/sap_mode_one/src/mode_one.cpp
@@ -0,0 +1,140 @@
+#include <ros/ros.h> // ROS 기본헤더파일
+#include "sap_mode_one/mode_msg.h" // mode_msg메시지파일헤더(빌드후자동생성됨)
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+//함수 선언
+bool moveToGoal(double xGoal, double yGoal);
+char choose();
+//모의 환경에서 사용하는 좌표 정보
+double xspot0=-0.1440;
+double yspot0=-0.1525;
+double xspot1=-0.0874;
+double yspot1=-1.3411;
+double xspot2=-0.5373;
+double yspot2=-0.6266;
+double xspot3=-0.4828;
+double yspot3=-1.5649;
+bool goalReached=false;
+
+class Mode{
+private:
+    ros::NodeHandle n;
+    ros::Subscriber mode_sub;
+    int mode_data;
+public:
+    Mode(ros::NodeHandle n);
+    ~Mode();
+    void modeMsgCallback(const sap_mode_one::mode_msg::ConstPtr& msg);
+    int getMode();
+};
+//Constructor
+Mode::Mode(ros::NodeHandle nh):n(nh){
+    mode_sub = n.subscribe("mode_msg", 100, &Mode::modeMsgCallback, this);
+}
+//Destructor
+Mode::~Mode(){}
+// 메시지콜백함수로써, 밑에서설정msg라는이름의토픽
+// 메시지를수신하였을때동작하는함수이다
+// 입력메시지로는sap_mode_one패키지의mode_msg메시지를받도록되어있다
+void Mode::modeMsgCallback(const sap_mode_one::mode_msg::ConstPtr &msg) {
+        ROS_INFO("Received data: %d", msg->data);
+        mode_data=msg->data;
+}
+int Mode::getMode(){
+    return mode_data;
+}
+
+int main(int argc, char** argv){
+        ros::init(argc, argv, "mode1_node");// 노드명초기화
+        ros::NodeHandle nh;// ROS 시스템과통신을위한노드핸들선언
+        int mode_data; //현재 선택된 모드
+	char choice='q';
+        Mode mode(nh);
+
+	while(true){
+		ros::spinOnce();
+		//사용자로부터 메뉴를 선택받는다.
+                ros::Duration(1).sleep();
+		mode_data= mode.getMode(); //현재 선택된 모드를 mode_data에 저장
+		ROS_INFO("mode= %d", mode_data); // mode값 표시한다
+		if(mode_data==1){
+		   
+		    choice=choose();
+		    if(choice=='0'){	//0번 스팟으로 이동
+		            goalReached=moveToGoal(xspot0,yspot0);
+		    }
+		    else if(choice=='1'){	//1번 스팟으로 이동
+		            goalReached=moveToGoal(xspot1,yspot1);
+		    }
+		    else if(choice=='2'){	//2번 스팟으로 이동
+		            goalReached=moveToGoal(xspot2,yspot2);
+		    }
+		    else if(choice=='3'){	//3번 스팟으로 이동
+		            goalReached=moveToGoal(xspot3,yspot3);
+		    }
+		    if(choice!='q'){
+		            if(goalReached){
+		                    ROS_INFO("Congratulations!");
+		                    ros::spinOnce();	//목적지 스팟에 도착하면 Congratulations! 출력
+		            }
+		            else{
+		                    ROS_INFO("Hard Luck!");	//정확한 주행 실패 시 Hard Luck! 출력
+		            }
+		    }
+		    
+		}
+
+
+	}
+        
+
+	return 0;
+}
+//특정 스팟 1개로 주행하는 함수이다.
+//파라미터: 목적지 스팟 좌표
+bool moveToGoal(double xGoal, double yGoal){
+//define a client for to send goal requests to the move_base server through a SimpleActionClient
+	actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ac("move_base", true);
+	//wait for the action server to come up
+	while(!ac.waitForServer(ros::Duration(5.0))){
+		ROS_INFO("Waiting for the move_base action server to come up");
+	}
+	move_base_msgs::MoveBaseGoal goal;
+	//set up the frame parameters
+	goal.target_pose.header.frame_id ="map";
+	goal.target_pose.header.stamp = ros::Time::now();
+	//목적지로 주행
+	goal.target_pose.pose.position.x =  xGoal;
+	goal.target_pose.pose.position.y =  yGoal;
+	goal.target_pose.pose.position.z =  0.0;
+	goal.target_pose.pose.orientation.x =0.0;
+	goal.target_pose.pose.orientation.y =0.0;
+	goal.target_pose.pose.orientation.z =0.0;
+	goal.target_pose.pose.orientation.w =1.0;
+	ROS_INFO("Sending goal location ...");
+	ac.sendGoal(goal);
+	ac.waitForResult();
+	if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){	//목적지에 성공적으로 도착했을 경우
+		ROS_INFO("You have reached the destination");
+		return true;
+	}
+	else{
+		ROS_INFO("The robot failed to reach the destination");	//목적지 도착을 실패했을 경우
+		return false;
+	}
+}
+//사용자가 목적지 스팟을 선택하는 함수이다.
+char choose(){
+	char choice='q';
+	std::cout<<"|-------------------------------|"<<std::endl;
+	std::cout<<"|원하는 스팟을 선택하세요."<<std::endl;
+	std::cout<<"|'0': Spot 0 "<<std::endl;
+	std::cout<<"|'1': Spot 1 "<<std::endl;
+	std::cout<<"|'2': Spot 2 "<<std::endl;
+	std::cout<<"|'3': Spot 3 "<<std::endl;
+	//std::cout<<"|'q': 종료 "<<std::endl;
+	std::cout<<"|-------------------------------|"<<std::endl;
+	std::cout<<"|WHERE TO GO?";
+	std::cin>>choice;
+	return choice;
+}
