0f37063edf640c374121b94a0934c8f1bdaac719
hyojin-shin <tlsgywls888@naver.com>
2021-06-25 12:56:13 +0900

    sap_mode_real
    최종 코드

---
+++ sap_mode_real/sap_mode_three/src/mode_three_demo.cpp
@@ -0,0 +1,471 @@
+#include <iostream>
+#include <ros/ros.h>
+#include "std_msgs/String.h"
+#include <mysql/mysql.h>
+#include <string>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include "sap_mode_three/Msg.h"
+#include "sap_mode_three/mode_msg.h" // mode_msg메시지파일헤더(빌드후자동생성됨)
+#include <unistd.h>
+#include <algorithm>
+
+
+MYSQL *connection = NULL;
+MYSQL_RES *result = NULL;
+MYSQL_ROW row;
+
+
+using namespace std;
+
+
+struct SpotInfo {
+
+  double x_cordinate;
+
+  double y_cordinate;
+
+  int status;
+
+  int dust_data;
+
+  int init_order;
+
+};
+
+//정렬 함수: 미세먼지 수치를 기준으로 내림차순 정렬한다.
+
+//미세먼지 수치가 같은 경우 스팟 번호 순서대로 오름차순 정렬한다.
+
+bool compare(const SpotInfo &p1, const SpotInfo &p2){
+
+  if(p1.dust_data>p2.dust_data)
+
+    return true;
+
+  else if(p1.dust_data==p2.dust_data)
+
+    //sorted by the sequence of array value order
+
+    return p1.init_order<p2.init_order;
+
+  else {
+
+    return false;
+
+  }
+
+}
+
+class Mode{
+private:
+  ros::NodeHandle n;
+  ros::Subscriber mode_sub;
+  int mode_data=0;
+public:
+  Mode(ros::NodeHandle n);
+  ~Mode();
+  void modeMsgCallback(const sap_mode_three::mode_msg::ConstPtr& m_msg);
+  int getMode();
+};
+//Constructor
+Mode::Mode(ros::NodeHandle nh):n(nh){
+  mode_sub = n.subscribe("mode_msg", 10, &Mode::modeMsgCallback, this);
+}
+//Destructor
+Mode::~Mode(){}
+// 메시지콜백함수로써, 밑에서설정msg라는이름의토픽
+// 메시지를수신하였을때동작하는함수이다
+// 입력메시지로는sap_mode_three패키지의mode_msg메시지를받도록되어있다
+void Mode::modeMsgCallback(const sap_mode_three::mode_msg::ConstPtr &m_msg) {
+  ROS_INFO("Received data: %d", m_msg->data);
+  mode_data=m_msg->data;
+}
+int Mode::getMode(){
+  return mode_data;
+}
+
+
+class Navigation{
+
+public:
+
+  Navigation();
+
+  ~Navigation();
+
+  void setSpot(SpotInfo* arr_p, SpotInfo* current);
+
+  bool moveToGoal(double xGoal, double yGoal);
+
+};
+
+//Constructor
+
+Navigation::Navigation(){}
+
+//Destructor
+
+Navigation::~Navigation() {}
+
+void Navigation::setSpot(SpotInfo* arr_p, SpotInfo* current) {
+
+  //initialize spots' status
+
+  for (int i =0; i<4; i++) {
+
+    //current->status = 0;
+
+    current->dust_data =0;
+
+    current++;
+
+  }
+
+  current = arr_p;
+
+  //put real cordinates into array
+
+  current->init_order=0;
+
+  current->x_cordinate =-1.2496;
+
+  current->y_cordinate =-3.395;
+
+  current++;
+
+  current->init_order=1;
+
+  current->x_cordinate =-0.8941;
+
+  current->y_cordinate =-0.5606;
+
+  current++;
+
+  current->init_order=2;
+
+  current->x_cordinate =0.0365;
+
+  current->y_cordinate =-1.8533;
+
+  current++;
+
+  current->init_order=3;
+
+  current->x_cordinate =0.3837;
+
+  current->y_cordinate =-0.0119;
+
+  for (int i =0; i<4; i++) {
+
+    cout <<"spot["<< i <<"].x_cordinate : "<< arr_p->x_cordinate <<endl;
+
+    cout <<"spot["<< i <<"].y_cordinate : "<< arr_p->y_cordinate <<endl;
+
+    arr_p++;
+
+  }
+
+}
+
+bool Navigation::moveToGoal(double xGoal, double yGoal) {
+
+  actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ac("move_base", true);
+
+  while (!ac.waitForServer(ros::Duration(5.0))) {
+
+    ROS_INFO("Waiting for the move_base action server to come up");
+
+  }
+
+  move_base_msgs::MoveBaseGoal goal;
+
+  goal.target_pose.header.frame_id ="map";
+
+  goal.target_pose.header.stamp = ros::Time::now();
+
+  goal.target_pose.pose.position.x = xGoal;
+
+  goal.target_pose.pose.position.y = yGoal;
+
+  goal.target_pose.pose.position.z =0.0;
+
+  goal.target_pose.pose.orientation.x =0.0;
+
+  goal.target_pose.pose.orientation.y =0.0;
+
+  goal.target_pose.pose.orientation.z =0.0;
+
+  goal.target_pose.pose.orientation.w =1.0;
+
+  ROS_INFO("Sending goal location ...");
+
+  cout << goal.target_pose.pose.position.x <<endl;
+
+  cout << goal.target_pose.pose.position.y <<endl;
+
+  cout << xGoal <<","<< yGoal <<endl;
+
+  ac.sendGoal(goal);
+
+  ac.waitForResult();
+
+  if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+
+    ROS_INFO("You have reached the destination");
+
+    return true;
+
+  }
+
+  else {
+
+    ROS_INFO("The robot failed to reach the destination");
+
+    return false;
+
+  }
+
+}
+
+class Subscriber{
+
+private:
+
+  ros::NodeHandle n;
+
+  ros::Subscriber sub;
+
+  int current_data;
+
+public:
+
+  Subscriber(ros::NodeHandle n);
+
+  ~Subscriber();
+
+  void msgCallback(const sap_mode_three::Msg::ConstPtr& msg);
+
+  int getMsg();
+
+};
+
+//Constructor
+
+Subscriber::Subscriber(ros::NodeHandle nh):n(nh){
+
+  sub = n.subscribe("pmsdata", 10, &Subscriber::msgCallback, this);
+
+}
+
+//Destructor
+
+Subscriber::~Subscriber(){}
+
+void Subscriber::msgCallback(const sap_mode_three::Msg::ConstPtr &msg) {
+
+  cout<<"Received data: "<< msg->pmsdata<<endl;
+
+  current_data=msg->pmsdata;
+
+}
+
+int Subscriber::getMsg(){
+
+  return current_data;
+
+}
+
+int main(int argc, char** argv) {
+
+  ros::init(argc, argv, "mode3_node");
+
+  ros::NodeHandle nh;
+
+  double x, y;
+
+  bool goalReached;
+
+  int current_data;
+
+  int loop_time=0;
+
+  int arr[15]; // 미세먼지 수치 값 평균 구하기 위한 배열
+
+  int sum =0; //미세먼지 수치 값 평균 구하기 위한 합
+
+  int avg; // 미세먼지 수치 평균값
+  int mode_data=0;
+  Mode mode(nh);
+
+  SpotInfo spot[4];
+
+  SpotInfo *p, *current_p;
+
+  Navigation navigation;
+
+  Subscriber subscriber(nh);
+
+  while(true){
+    ros::spinOnce();
+    ros::Duration(1).sleep();
+
+    mode_data=mode.getMode();
+    ROS_INFO("mode= %d",mode_data);
+
+    if(mode_data==3){
+
+      p = spot;
+
+      current_p = spot;
+
+      navigation.setSpot(p, current_p);
+
+      /////////////////////dbcheck///////////////////////////////////////////////
+
+      connection = mysql_init(connection);
+      mysql_real_connect(connection, NULL, "newhj", "shj4197", "testdb", 0, NULL, 0);
+      if(mysql_query(connection, "SELECT * FROM test_table")){
+        ROS_INFO("Query Error: %s", mysql_error(connection));
+        exit(1);
+      }
+
+      else{
+        result = mysql_use_result(connection);
+        for(int i=0; i < mysql_field_count(connection); ++i){
+          std::stringstream ss;
+          row = mysql_fetch_row(result);
+          if(row <= 0){
+            break;
+          }
+          for(int j=0; j < mysql_num_fields(result); ++j){
+            ss << row[j] << " ";
+          }
+          ROS_INFO("%s", ss.str().c_str());
+        }
+        mysql_free_result(result);
+      }
+
+
+      ///////////////////////////////////////////////////////////////////////////
+
+
+      for (int i =0; i<4; i++,current_p++) {
+
+        x = current_p->x_cordinate;
+
+        y = current_p->y_cordinate;
+
+        cout <<"go to x :"<< x <<", y :"<< y <<endl;
+
+        goalReached = navigation.moveToGoal(x, y);
+
+        if (goalReached) {
+
+          ROS_INFO("Congratulations!");
+
+          ros::Duration(10).sleep();
+
+          for(int j=0;j<15;j++){	//스팟 내에서 미세먼지 데이터 15개를 배열 arr[]에 저장한다.
+
+            ros::spinOnce();	//버퍼에 있는 미세먼지 데이터를 받아옴
+
+            current_data=subscriber.getMsg();	//가장 최근 데이터를 current_data에 저장
+
+            ROS_INFO("current_data : %d", current_data);
+
+            arr[j]=current_data;	//배열에 저장한다.
+
+            usleep(1000000);	//1초 delay
+
+          }
+
+          for(int j=0;j<15;j++){
+
+            sum=sum+arr[j];
+
+          }
+
+          avg=sum/15;	//배열에 담겨있는 데이터의 평균을 구한다.
+
+          current_p->dust_data=avg;	//평균값을 구조체에 해당 스팟의 dust_data에 저장한다.
+
+          ROS_INFO("AVG: %d",current_p->dust_data);	//평균값 출력
+
+
+          //////////////////////////////////////db insert/////////////////////////////
+
+          std::string query="INSERT INTO test_table (spot, pmsdata, date) VALUES ('"+to_string(i)+"', '"+to_string(avg)+"', NOW());";
+
+          mysql_query(connection,query.c_str());
+
+          ////////////////////////////////////////////////////////////////////////////
+
+          sum=0;
+
+
+
+        }
+
+        else
+
+          ROS_INFO("Hard Luck!");
+
+      }
+
+      //미세먼지 수치를 바탕으로 정렬한다.
+
+      sort(p,p+4,compare);
+
+      // /////////////sorting check/////////////////////////////
+
+      //정렬이 성공적으로 되었는지 확인한다.
+
+      current_p=p;
+
+      for(int i=0;i<4;i++,current_p++){
+
+        cout<<"----sorting print----"<<endl;
+
+        cout<<"x: "<<current_p->x_cordinate<<endl;
+
+        cout<<"y: "<<current_p->y_cordinate<<endl;
+
+        cout<<"dust: "<<current_p->dust_data<<endl;
+
+        cout<<"---------------------"<<endl;
+
+      }
+
+      // ///////////////////////////////////////////////////////
+
+      x=p->x_cordinate;
+
+      y=p->y_cordinate;	//x,y는 최고 미세먼지 수치 스팟의 좌표
+
+      cout <<"go to x :"<< x <<", y :"<< y <<endl;
+
+      cout<<"to the highest data : "<<p->dust_data<<endl;
+
+      goalReached = navigation.moveToGoal(x, y);		//최고 미세먼지 스팟으로 이동한다.
+
+      if (goalReached) {
+
+        ROS_INFO("Congratulations!");
+
+        ROS_INFO("~Purifying~");
+
+        ros::Duration(10).sleep();
+
+      }
+
+      else
+
+        ROS_INFO("Hard Luck!");
+
+    }
+  }
+
+  return 0;
+
+}
